d7b1adcdc9ce9f228ad24ee18511f905b5892f66
Luan Panini <luanoliver2603@gmail.com>
2022-04-04 19:47:35 -0300

    post

--- src/app/post/post.module.ts
+++ src/app/post/post.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-import { AlertController, IonicModule } from '@ionic/angular';
+import { IonicModule } from '@ionic/angular';
 
 import { PostPageRoutingModule } from './post-routing.module';
 
@@ -14,7 +14,7 @@ import { PostPage } from './post.page';
     FormsModule,
     IonicModule,
     PostPageRoutingModule,
-    AlertController
+
   ],
   declarations: [PostPage]
 })
