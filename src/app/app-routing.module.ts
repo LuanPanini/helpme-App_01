33fb27f95eb8159c0c2e90b0d6c7eb18b4fb539d
LuanPanini <luanoliver2603@gmail.com>
2022-04-04 06:46:59 -0300

    04.04.22

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -33,7 +33,11 @@ const routes: Routes = [
   {
     path: 'cadastro',
     loadChildren: () => import('./cadastro/cadastro.module').then( m => m.CadastroPageModule)
+  },  {
+    path: 'post',
+    loadChildren: () => import('./post/post.module').then( m => m.PostPageModule)
   }
+
 
 ];
 @NgModule({
